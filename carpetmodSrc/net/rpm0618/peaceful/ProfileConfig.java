d0caa3f50855e6f14cf7d6a24bf9050460998ef6
Ryan M <rpm0618@gmail.com>
2026-03-29 08:53:27 -0400

    Initial peaceful profiling

---
+++ carpetmodSrc/net/rpm0618/peaceful/ProfileConfig.java
@@ -0,0 +1,8 @@
+package net.rpm0618.peaceful;
+
+import net.minecraft.util.math.ChunkPos;
+
+public class ProfileConfig {
+    public static ChunkPos GLASS_CHUNK = new ChunkPos(353, -370);
+    public static ChunkPos REHASH_CHUNK = new ChunkPos(343, -398);
+}
